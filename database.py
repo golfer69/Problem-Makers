fb1389992d568576eee6032871d8a80f8f6bbd9b
golfer69 <1221109242@student.mmu.edu.my>
2024-01-10 22:37:39 +0800

    more changes to database

--- database.py
+++ database.py
@@ -1,33 +1,30 @@
-
-
 import sqlite3
 
-conn = sqlite3.connect('gamedata.db') #database
-cursor= conn.cursor() #interface to database
+conn = sqlite3.connect('gamedata.db')  # database
+cursor = conn.cursor()  # interface to database
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS Leaderboard (
-               ID_Number INTEGER,
                Contestants TEXT,
                Score INTEGER
-)
+               )
 """)
+#user inputs name and score
+contestants = input("Enter your name: ")
+score = int(input("Enter your score: "))
 
+#insert into contestant and score columns
 cursor.execute("""
-INSERT INTO Leaderboard VALUES
-               (1, 'Alif Akmal', 1005),
-               (2, 'Brian Ng', 3004),
-               (3, 'Schweeta', 5990)
-               """)
+INSERT INTO Leaderboard (Contestants, Score) 
+               VALUES (?, ?)
+               """, (contestants, score))
 
 cursor.execute("""
 SELECT * FROM Leaderboard
-               WHERE Contestants = 'Alif Akmal'
                """)
 
-rows=cursor.fetchone()
+rows = cursor.fetchall()
 print(rows)
 
-conn.commit
-conn.close
-
+conn.commit()  
+conn.close()
