a348876ceb5c5857c0f2f516983145729947495f
Brian Ng <144977561+brianngzhengyang@users.noreply.github.com>
2024-01-14 23:28:28 +0800

    Added the quiz part (not finish tho)

--- game.py
+++ game.py
@@ -10,11 +10,12 @@
 # *********************************************************
 import random
 import turtle
+import tkinter as tk
 
 ## Randomizer ##
 
 #Chooses types of shape for the question 
-shapes = ["square", "circle", "triangle"]
+shapes = ["Square", "Circle", "Triangle"]
 shape_1 = random.choice(shapes)
 shape_2 = random.choice(shapes)
 shape_3 = random.choice(shapes)
@@ -23,7 +24,7 @@ chosen_shapes = [shape_1, shape_2, shape_3]
 #Chooses how many shapes will be drawn 
 random_num_shapes = random.randint(1, 2)	
 
-## Draw functions ## ALL OF THESE DRAWINGS ARE JUST FOR TESTING, CAN CHANGE 
+## Draw functions ## 
 t = turtle.Turtle()
 t.speed(0)
 side_length = 50 #sample sidelength can change in the future
@@ -60,7 +61,6 @@ def draw_circle():
 # Shapes counter ##
 shape_counter = {}  # Initialize an empty dictionary for counts
 
-# Updated upstream
 def count(shape_counter, shape):
     if shape in shape_counter:
         shape_counter[shape] += 1  # Add one count if shape exists
@@ -72,17 +72,12 @@ def printcount():
     for shape, count in shape_counter.items():
      print(f"- {shape}: {count}")
 
-
-
 ## Coordinates ##
 base_coordinates1 = (0, 0)
 base_coordinates2 = (100, 0)
 
 ## Drawing ##
 #Random pick chosen shapes and record the shape picked
-
-
-# count(shape_counter, shape)
 
 #Call draw function
 
@@ -91,11 +86,11 @@ def draw(chosen_shapes):
 
     shape = random.choice(chosen_shapes)
 
-    if shape == "triangle":
+    if shape == "Triangle":
         draw_triangle()
-    elif shape == "square":
+    elif shape == "Square":
         draw_square()
-    elif shape == "circle":
+    elif shape == "Circle":
         draw_circle()
     count(shape_counter, shape)
 
@@ -107,10 +102,35 @@ elif random_num_shapes == 2:
   draw(chosen_shapes)
   t.goto(base_coordinates2[0], base_coordinates2[1])
   draw(chosen_shapes)
+  
+## Quiz ##
 
+# window and score
+window = tk.Tk()
+window.geometry("500x300")
+window.title("Shapey | Quiz window")
+score = 0
+questions = [
+    {"question": "What shape is it?", "answers": ["Triangle","Circle","Square"], "correct": list(shape_counter.keys())[0]},
+]
+#prints question 
+question_label = tk.Label(window, text=["question"])
+question_label.pack(pady=20)
+#create answers button and respond to clicked button
+answer_buttons = []
+for answer in questions[0]["answers"]: 
+    button = tk.Button(window, text=answer, command=lambda text=answer: check_answer(text))
+    button.pack(pady=5)
+    answer_buttons.append(button)
+#check ans
+def check_answer(text):
+    if text == questions[0]["correct"]:
+        print("congrats ig LOL")
+    else:
+       print(text)
+       print(f"the ans is {questions[0]["correct"]}" )
+
+window.mainloop()
 printcount()
 turtle.mainloop()
-
-
-
 
